f3d4c6da06c656bde3c22fb82b0610b7c3cc3ffe
jbae11@illinois.edu <jbae11@illinois.edu>
2018-05-18 19:36:31 -0400

    it works now

--- udb_reactor/udb_reactor.py
+++ udb_reactor/udb_reactor.py
@@ -32,42 +32,45 @@ class udb_reactor(Facility):
         super().__init__(*args, **kwargs)
 
     def write(self, string):
+        # for debugging purposes.
         with open('log.txt', 'a') as f:
             f.write(string + '\n')
 
-    def tock(self):
-        self.write('what')
 
-        # Example dummy code
+    def tock(self):
+        self.write('Tock')
+        # Example dummy material
+        # this should be replaced with material
+        # from the UDB database
         composition = {922350000: 5,
                        922380000: 95}
         material = ts.Material.create(self, 100, composition)
 
+        # stores the planned spent fuel into inventory buffer
+        # for it to be offered to the market
         self.inventory.push(material)
         self.write(str(self.inventory.quantity))
 
 
-    #     time = self.context.time
-    #     get rows that match with current time
-
-    #     for information in rows:
-    #         Create material given by recipe and quantity
-    #         composition = {ZZAAA0000: massfrac,
-    #                        ZZAAA0000: massfrac}
-    #         recipe = self.context.get_recipe()
-    #         material = ts.Material.create(self, quantity, recipe)
-
-    #         Push material to out buffer
-    #         self.out.push(material)
-
-
     def get_material_bids(self, requests):
+        """ Gets material bids that want its `outcommod' an
+            returns bid portfolio
+        """
         if self.outcommod not in requests:
             return
         reqs = requests[self.outcommod]
-        bids = [reqs]
-        ports = [{"bids": bids, "constraints": self.inventory.quantity}]
-        return ports
+        bids = []
+        for req in reqs:
+            qty = min(req.target.quantity, self.inventory.quantity)
+            # returns if the inventory is empty
+            if self.inventory.empty():
+                return
+            # get the composition of the material next in line
+            next_in_line = self.inventory.peek()
+            mat = ts.Material.create_untracked(qty, next_in_line.comp())
+            bids.append({'request': req, 'offer': mat})
+        port = {"bids": bids}
+        return port
 
 
     def get_material_trades(self, trades):
